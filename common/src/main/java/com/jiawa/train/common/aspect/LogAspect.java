4a50c6ebde3c4430543ffdbd59e1f30a70e65f48
2cc <1607042937@qq.com>
2024-01-08 15:59:49 +0800

    4.6增加公共模块common,用于放置公共类,比如工具类,拦截器，AOP,常量,枚举,公共配置等

--- common/src/main/java/com/jiawa/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/jiawa/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.jiawa.train.member.aspect;
+package com.jiawa.train.common.aspect;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
