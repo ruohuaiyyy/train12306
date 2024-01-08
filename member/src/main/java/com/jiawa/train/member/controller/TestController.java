35c9832105a133a04bb3e6fcead376eb6b50785b
2cc <1607042937@qq.com>
2024-01-08 14:53:13 +0800

    新增member会员模块

--- member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -3,15 +3,10 @@ package com.jiawa.train.member.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Author: lzj
- * @Description:
- * @Date: Created in 17:24 2024/1/4
- */
 @RestController
 public class TestController {
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello Word!";
+    public String hello() {
+        return "Hello World!";
     }
 }
